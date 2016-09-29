58798fbd01a826f1149820bbdfa7d8f1f19a8214
lixinliang <lixinliang@yy.com>
2016-09-29 20:48:43 +0800

    use new webpack flow
    use new webpack flow

--- webpack/webpack.build.js
+++ webpack/webpack.build.js
@@ -3,9 +3,12 @@
 let path = require('path');
 let moment = require('moment');
 let webpack = require('webpack');
-let PackageInfo = require('./package-info.js');
-let packageJson = require('../package.json');
-let extractTextPlugin = require('extract-text-webpack-plugin');
+let Profile = require('./webpack.profile.js');
+let ExtractText = require('extract-text-webpack-plugin');
+
+const alias = {};
+const entry = require('./webpack.entry.json');
+const packageJson = require('../package.json');
 
 const banner =
 `@ProjectName ${ packageJson.name }
@@ -13,20 +16,18 @@ const banner =
 @Author ${ packageJson.author.name }(${ packageJson.author.url })
 @Update ${ moment().format('YYYY-MM-DD h:mm:ss a') }`;
 
-let entry = {};
-let alias = {};
-
-entry[packageJson.name] = require('./entry.js')[packageJson.name];
+const imageSize = 10240;
 
 module.exports = {
-    entry : entry,
+    entry,
     output : {
-        path : './dist/',
-        filename : '[name].min.js',
-        library : packageJson.name.replace(/-./g, ( match ) => match[1].toUpperCase()),
-        libraryTarget : 'umd',
+        path : './dist/js/',
+        filename : '[name].js',
     },
-    extensions: ['.vue', '.js', '.json', '.scss', '.html'],
+    extensions : ['.vue', '.js', '.json', '.scss', '.html'],
+    resolve : {
+        alias,
+    },
     module : {
         loaders : [
             {
@@ -39,15 +40,15 @@ module.exports = {
             },
             {
                 test : /\.(png|jpg|gif|svg)$/,
-                loader : 'url?limit=10240&name=../img/[name].[ext]?[hash]',
+                loader : `url?limit=${ imageSize }&name=../img/[name].[ext]?[hash]`,
             },
             {
                 test : /\.css$/,
-                loader : extractTextPlugin.extract('style', 'css'),
+                loader : ExtractText.extract('style', 'css'),
             },
             {
                 test : /\.scss$/,
-                loader : extractTextPlugin.extract('style', 'css?localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass'),
+                loader : ExtractText.extract('style', 'css?localIdentName=[local]___[hash:base64:5]!autoprefixer?browsers=last 2 version!sass'),
             },
             {
                 test : /\.js$/,
@@ -55,27 +56,51 @@ module.exports = {
                 loader : 'babel',
                 query : {
                     presets : ['es2015', 'stage-0'],
+                    // plugins : ['transform-remove-strict-mode'],
                     // plugins: ['transform-runtime'],
                 },
             },
         ],
     },
     plugins : [
-        new webpack.BannerPlugin(banner),
-        new webpack.optimize.UglifyJsPlugin({
-            compress : {
-                warnings : false
+        new ExtractText('../css/[name].css'),
+        new webpack.Profile(),
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV : '"production"',
             },
         }),
-        new PackageInfo({
-            '@NAME' : packageJson.name,
-            '@VERSION' : packageJson.version,
+        new webpack.optimize.UglifyJsPlugin({
+            compress : {
+                warnings : false,
+            },
+            output : {
+                comments : false,
+            },
         }),
+        new webpack.BannerPlugin(banner),
     ],
     vue : {
         loaders : {
-            sass : extractTextPlugin.extract('style', 'css!autoprefixer?browsers=last 2 version!sass?indentedSyntax'),
-            scss : extractTextPlugin.extract('style', 'css!autoprefixer?browsers=last 2 version!sass'),
+            sass : ExtractText.extract('style', 'css!autoprefixer?browsers=last 2 version!sass?indentedSyntax'),
+            scss : ExtractText.extract('style', 'css!autoprefixer?browsers=last 2 version!sass'),
         }
     },
 };
+
+if (process.argv[process.argv.length-1] == '--build=js') {
+    let output = {
+        path : './dist/',
+        filename : '[name].min.js',
+    };
+    module.exports.plugins.shift();
+    let loaders = module.exports.module.loaders;
+    loaders[3].loaders = ['css', 'autoprefixer'];
+    loaders[4].loaders = ['css', 'autoprefixer', 'sass'];
+    loaders[3].loader = loaders[4].loader = void 0;
+    process.argv.slice(-3).slice(0, 2).forEach(( argument ) => {
+        let result = argument.trim().replace('--', '').split('=');
+        output[result[0]] = result[1];
+    });
+    module.exports.output = output;
+}
